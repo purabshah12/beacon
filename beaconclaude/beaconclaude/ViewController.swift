47fd3a7f9438b932f8dfe44e881a704f3811eeac
eswar-karavadi <125041114+eswar-karavadi@users.noreply.github.com>
2025-11-07 18:34:49 -0500

    init commit swift proj

---
+++ beaconclaude/beaconclaude/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beaconclaude
+//
+//  Created by Eswar Karavadi on 11/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ beaconclaude/beaconclaudeTests/beaconclaudeTests.swift
@@ -0,0 +1,17 @@
+//
+//  beaconclaudeTests.swift
+//  beaconclaudeTests
+//
+//  Created by Eswar Karavadi on 11/7/25.
+//
+
+import Testing
+@testable import beaconclaude
+
+struct beaconclaudeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
